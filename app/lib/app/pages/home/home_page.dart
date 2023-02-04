ad30e6be8bfcffef9e4a8631ee26855206b9bc29
Nadilson <nadilson@protonmail.com>
2023-02-03 22:28:04 -0300

    BLoC, preparando estado, status da tela, link entre controller e repository, tela Home

--- app/lib/app/pages/home/home_page.dart
+++ app/lib/app/pages/home/home_page.dart
@@ -1,8 +1,11 @@
 import 'package:dw9_delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/home/home_controller.dart';
+import 'package:dw9_delivery_app/app/pages/home/home_state.dart';
 import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../core/ui/helpers/loader.dart';
 
@@ -14,6 +17,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> with Loader, Messages {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      context.read<HomeController>().loadProducts();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,25 +40,39 @@ class _HomePageState extends State<HomePage> with Loader, Messages {
         await Future.delayed(Duration(seconds: 2));
         showSuccess('Teste com snackbar de sucesso em operações no app');
       }), */
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return DeliveryProductTile(
-                    product: ProductModel(
-                        id: 0,
-                        name: 'Lanche X',
-                        description:
-                            'Lanche acompanha pão, hambúguer, mussarela e maionese',
-                        price: 15.0,
-                        image:
-                            'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800'));
-              },
-            ),
-          ),
-        ],
+      body: BlocConsumer<HomeController, HomeState>(
+        listener: (context, state) {
+          state.status.matchAny(
+            any: () => hideLoader(),
+            loading: () => showLoader(),
+            error: () {
+              hideLoader();
+              showError(state.errorMessage ?? 'Erro desconhecido!');
+            },
+          );
+        },
+        buildWhen: (previous, current) => current.status.matchAny(
+          any: () => false,
+          initial: () => true,
+          loaded: () => true,
+        ),
+        builder: (context, state) {
+          return Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: state.products.length,
+                  itemBuilder: (context, index) {
+                    final products = state.products[index];
+                    return DeliveryProductTile(
+                      product: products,
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
