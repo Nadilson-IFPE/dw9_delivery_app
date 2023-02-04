f2e12fb7c153c3e659f320eb40790d47af7623a5
Nadilson <nadilson@protonmail.com>
2023-02-04 12:17:30 -0300

    Tela de detalhe do produto, botão Increment-Decrement, rotas

--- app/lib/app/dw9_delivery_app.dart
+++ app/lib/app/dw9_delivery_app.dart
@@ -1,6 +1,8 @@
 import 'package:dw9_delivery_app/app/core/provider/application_binding.dart';
 import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/home/home_router.dart';
+import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_page.dart';
+import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_router.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -16,6 +18,7 @@ class Dw9DeliveryApp extends StatelessWidget {
         routes: {
           '/': (context) => SplashPage(),
           '/home': (context) => HomeRouter.page,
+          '/productDetail': (context) => ProductDetailRouter.page,
         },
       ),
     );
