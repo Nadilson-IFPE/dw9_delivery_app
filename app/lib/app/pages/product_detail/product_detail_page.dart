c0285af550bfad05b940999c99cf73664ecdab35
Nadilson <nadilson@protonmail.com>
2023-02-04 15:02:04 -0300

    Modelo de OrderProduct, implementação da tela de detalhe do produto

--- app/lib/app/pages/product_detail/product_detail_page.dart
+++ app/lib/app/pages/product_detail/product_detail_page.dart
@@ -1,13 +1,30 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:dw9_delivery_app/app/core/extensions/formatter_extension.dart';
+import 'package:flutter/material.dart';
+
+import 'package:dw9_delivery_app/app/core/ui/base_state/base_state.dart';
 import 'package:dw9_delivery_app/app/core/ui/helpers/size_extensions.dart';
 import 'package:dw9_delivery_app/app/core/ui/styles/text_styles.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_increment_decrement_button.dart';
-import 'package:flutter/material.dart';
+import 'package:dw9_delivery_app/app/models/product_model.dart';
+import 'package:dw9_delivery_app/app/pages/product_detail/product_detail_controller.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class ProductDetailPage extends StatelessWidget {
-  const ProductDetailPage({super.key});
+class ProductDetailPage extends StatefulWidget {
+  final ProductModel productModel;
 
+  const ProductDetailPage({
+    super.key,
+    required this.productModel,
+  });
+
+  @override
+  State<ProductDetailPage> createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState
+    extends BaseState<ProductDetailPage, ProductDetailController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +35,9 @@ class ProductDetailPage extends StatelessWidget {
           Container(
             width: context.screenWidth,
             height: context.percentHeight(.4),
-            decoration: const BoxDecoration(
+            decoration: BoxDecoration(
               image: DecorationImage(
-                image: NetworkImage(
-                    'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800'),
+                image: NetworkImage(widget.productModel.image),
                 fit: BoxFit.cover,
               ),
             ),
@@ -32,19 +48,19 @@ class ProductDetailPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 10),
             child: Text(
-              'X-Burger',
+              widget.productModel.name,
               style: context.textStyles.textExtraBold.copyWith(fontSize: 22),
             ),
           ),
           const SizedBox(
             height: 10,
           ),
-          const Expanded(
+          Expanded(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 10),
               child: SingleChildScrollView(
                 child: Text(
-                  'Lanche acompanha pão, hambúguer, mussarela e maionese.',
+                  widget.productModel.description,
                 ),
               ),
             ),
@@ -56,35 +72,51 @@ class ProductDetailPage extends StatelessWidget {
                 width: context.percentWidth(.5),
                 height: 68,
                 padding: EdgeInsets.all(8),
-                child: const DeliveryIncrementDecrementButton(),
+                child: BlocBuilder<ProductDetailController, int>(
+                  builder: (context, amount) {
+                    return DeliveryIncrementDecrementButton(
+                      amount: amount,
+                      decrementTap: () {
+                        controller.decrement();
+                      },
+                      incrementTap: () {
+                        controller.increment();
+                      },
+                    );
+                  },
+                ),
               ),
               Container(
                 width: context.percentWidth(.5),
                 height: 68,
                 padding: EdgeInsets.all(8),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Adicionar',
-                        style: context.textStyles.textExtraBold
-                            .copyWith(fontSize: 13),
+                child: BlocBuilder<ProductDetailController, int>(
+                  builder: (context, amount) {
+                    return ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Adicionar',
+                            style: context.textStyles.textExtraBold
+                                .copyWith(fontSize: 13),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          AutoSizeText(
+                            (widget.productModel.price * amount).currencyPTBR,
+                            maxFontSize: 13,
+                            minFontSize: 5,
+                            maxLines: 1,
+                            textAlign: TextAlign.center,
+                            style: context.textStyles.textExtraBold,
+                          ),
+                        ],
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      AutoSizeText(
-                        r'R$ 60,99',
-                        maxFontSize: 13,
-                        minFontSize: 5,
-                        maxLines: 1,
-                        textAlign: TextAlign.center,
-                        style: context.textStyles.textExtraBold,
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
               ),
             ],
